14e647a13b88dab853ac34116c332009b3a723bc
dankito <git@dankito.net>
2020-10-18 23:21:49 +0200

    Moved BankFinderService to service directory

--- src/app/services/bankfinder.service.ts
+++ src/app/services/bankfinder.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
-import { Bank } from "./model/bank";
+import { Bank } from "../model/bank";
 
 
 @Injectable({
